a51f71f2190040a490397a25f476f1abde92ffc3
Md Roknuzzaman Rasel <rasel@graduate.utm.my>
2024-09-04 01:53:51 +0600

    data fetch added and create the frame for order and card

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import './App.css'
-import Header from './components/Header/Header'
-import Navbar from './components/Navbar/Navbar'
-import Title from './components/Title/title';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Recipes from "./components/Recipes/Recipes";
+import Title from "./components/Title/title";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setRecipes(data));
+  }, []);
+
   return (
     <div className="container  mx-auto font-lexend">
-      <Navbar/>
-      <Header/>
-      <Title/>
+      <Navbar />
+      <Header />
+      <Title />
+      <Recipes recipes={recipes}/>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,9 +1,20 @@
-const Recipes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import PropTypes from "prop-types"; // ES6
+
+const Recipes = ({ recipes }) => {
+  return (
+    <div className="flex">
+      <div className="w-3/4">
+        <h1>Card</h1>
+        <h1>Length: {recipes.length}</h1>
+      </div>
+      <div className="w-1/4">
+        <h1>order</h1>
+      </div>
+    </div>
+  );
 };
 
+Recipes.propTypes = {
+  recipes: PropTypes.array.isRequired,
+};
 export default Recipes;
