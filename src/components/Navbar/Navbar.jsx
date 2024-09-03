d7a7090b2f934827e07c6aee89f9a9e465da703e
Md Roknuzzaman Rasel <rasel@graduate.utm.my>
2024-09-04 01:10:26 +0600

    add search button and solve others design issue

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-// import { CiSearch } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
 import { FaRegUserCircle } from "react-icons/fa";
 const Navbar = () => {
   return (
@@ -13,14 +13,15 @@ const Navbar = () => {
         <a className="btn btn-ghost">Search</a>
       </div>
       <div className="navbar-end">
+        <button className="text-2xl absolute m-16"><CiSearch/></button>
         <input
           type="text"
           placeholder="search here"
           className="border p-2 pl-4 rounded-3xl"
         />
-        <div
-          className="ml-4 bg-[#0BE58A] p-2 rounded-full"
-        >
+        
+        
+        <div className="ml-4 bg-[#0BE58A] p-2 rounded-full">
           <FaRegUserCircle className="text-2xl" />
         </div>
       </div>
