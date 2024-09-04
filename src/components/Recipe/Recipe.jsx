77cf20ed0ec5fd041e7706a3111b83893d24f41c
Md Roknuzzaman Rasel <rasel@graduate.utm.my>
2024-09-04 14:43:22 +0600

    card added

---
+++ src/components/Recipe/Recipe.jsx
@@ -0,0 +1,66 @@
+import PropTypes from "prop-types"; // ES6
+import { FiClock } from "react-icons/fi";
+import { GoFlame } from "react-icons/go";
+
+const Recipe = ({ recipe }) => {
+  const {
+    recipe_image,
+    recipe_name,
+    short_description,
+    ingredients,
+    preparing_time,
+    calories,
+  } = recipe;
+  console.log(recipe);
+  return (
+    <div className="border rounded-2xl">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <figure className="px-4 pt-4">
+          <img
+            src={recipe_image}
+            alt={recipe_name}
+            className="rounded-xl w-full h-56"
+          />
+        </figure>
+        <div className="px-4 py-4">
+          <h2 className="text-xl font-semibold">{recipe_name}</h2>
+          <p className="text-[#878787] pb-2">{short_description}</p>
+
+          <hr className="pb-2" />
+          <h2 className="text-base">Ingredients: {ingredients.length} </h2>
+
+          <ul className="pb-2">
+            {ingredients.map((ingredient, index) => (
+              <li className="text-[#878787] list-disc mx-4 text-sm" key={index}>
+                {ingredient}
+              </li>
+            ))}
+          </ul>
+
+          <hr className="pt-2" />
+
+          <div className="flex gap-8 text-[#282828cc] ">
+            <div className="flex items-center p">
+              <FiClock />
+              <p className="pl-2">{preparing_time} minutes</p>
+            </div>
+            <div className="flex items-center p">
+              <GoFlame />
+              <p className="pl-2">{calories} calories</p>
+            </div>
+          </div>
+          <div className="card-actions pt-2">
+            <button className="btn bg-[#0BE58A] rounded-3xl">
+              Want to cook
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Recipe.propTypes = {
+  recipe: PropTypes.object.isRequired,
+};
+export default Recipe;
--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,11 +1,14 @@
 import PropTypes from "prop-types"; // ES6
+import Recipe from "../Recipe/Recipe";
 
 const Recipes = ({ recipes }) => {
   return (
     <div className="flex">
-      <div className="w-3/4">
-        <h1>Card</h1>
-        <h1>Length: {recipes.length}</h1>
+      <div className="w-3/4 flex flex-wrap gap-4">
+        {/* <h1>Length: {recipes.length}</h1> */}
+        {recipes.map((recipe) => (
+          <Recipe key={recipe.recipe_id} recipe={recipe}></Recipe>
+        ))}
       </div>
       <div className="w-1/4">
         <h1>order</h1>
